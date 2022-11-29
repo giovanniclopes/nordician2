b0965cbb57382045b036e65908dc17c1fa54f96a
Giovanni Lopes <giovanniclopes@gmail.com>
2022-11-29 00:29:44 -0300

    CTA created on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,24 +5,29 @@ export function Home() {
     <>
       <Navbar />
       <div className="h-fit bg-main bg-cover">
-        <div className="flex items-center justify-center py-40">
-          <div className="flex gap-8 flex-col font-nordica font-bold">
-            <div>
-              <h1 className="text-white text-6xl">
+        <div className="flex items-center justify-center py-40 sm:py-32">
+          <div className="flex flex-col items-center justify-center gap-8 font-nordica font-bold">
+            <div className="flex flex-col items-center justify-center gap-4">
+              <h1 className="text-white text-8xl text-center sm:text-5xl">
                 Boas-vindas ao Mundo Nórdico
               </h1>
             </div>
-            <div className="flex items-center justify-center gap-6 text-gray-normal">
-              <a href="">
-                <button className="bg-red-main border-[3px] border-transparent rounded-md text-3xl py-4 px-14 transition-all hover:bg-opacity-80">
-                  Mitologia
-                </button>
-              </a>
-              <a href="">
-                <button className="bg-transparent border-[3px] border-red-main rounded-md text-red-main text-3xl py-4 px-14 transition-all hover:border-opacity-80 hover:text-opacity-80">
-                  Realidade
-                </button>
-              </a>
+            <div className="flex flex-col items-center justify-center gap-6 text-gray-normal">
+              <p className="text-white text-opacity-50 text-xl font-light">
+                Escolha uma opção para saber mais
+              </p>
+              <div className="flex flex-row gap-6 items-center justify-center">
+                <a href="">
+                  <button className="bg-red-main border-[3px] border-transparent rounded-md text-3xl py-4 px-14 transition-all hover:bg-opacity-80 sm:text-2xl sm:px-9 sm:py-3">
+                    Mitologia
+                  </button>
+                </a>
+                <a href="">
+                  <button className="bg-transparent border-[3px] border-red-main rounded-md text-red-main text-3xl py-4 px-14 transition-all hover:border-opacity-80 hover:text-opacity-80 sm:text-2xl sm:px-9 sm:py-3">
+                    Realidade
+                  </button>
+                </a>
+              </div>
             </div>
           </div>
           <div></div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,22 @@ module.exports = {
   content: ["./src/**/**.tsx"],
   theme: {
     extend: {
+      screens: {
+        "2xl": { max: "1535px" },
+        // => @media (max-width: 1535px) { ... }
+
+        xl: { max: "1279px" },
+        // => @media (max-width: 1279px) { ... }
+
+        lg: { max: "800px" },
+        // => @media (max-width: 1023px) { ... }
+
+        md: { max: "767px" },
+        // => @media (max-width: 767px) { ... }
+
+        sm: { max: "620px" },
+        // => @media (max-width: 639px) { ... }
+      },
       backgroundImage: {
         main: "url(/src/assets/background.webp)",
         gradient: "linear-gradient(-150deg, #131313, #000000, #131313)",
@@ -17,7 +33,7 @@ module.exports = {
         },
         red: {
           main: "#B70000",
-        }
+        },
       },
     },
   },
