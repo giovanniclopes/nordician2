4e9d403cdc15750cf8dfde426b1b80ba133c4013
Giovanni Lopes <giovanniclopes@gmail.com>
2022-12-02 01:53:11 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -38,8 +38,8 @@ export function Home() {
         className="flex flex-col items-center justify-center gap-6 p-12"
       >
         <hr className="border border-maroon-100 w-2/3 mb-10" />
-        <p className="text-center w-3/4">
-          Olá viajante, bem vinda(o) ao site Nordician! <br />
+        <p className="text-center w-3/4 mbl:w-auto">
+          Olá viajante, bem vinda(o) ao Nordician! <br />
           Aqui forneço diversos artigos para quem busca por informações sobre a
           cultura nórdica. <br /> Contendo artigos sobre o mundo real, e sobre o
           mundo místico. <br /> No topo/menu deste site, você encontrará os
@@ -48,7 +48,7 @@ export function Home() {
           <span className="text-maroon-100">'Realidade'</span>, onde estão
           localizados todo o conteúdo nórdico. Aproveite! <br />
         </p>
-        <p className="text-center w-3/4 text-maroon-100">
+        <p className="text-center w-3/4 text-maroon-100 mbl:w-auto">
           E lembrando! os textos de todos os artigos podem estar no idioma
           Dinamarquês ou Norueguês, para preservar a cultura e dar um aspecto
           mais nórdico. Porém você pode facilmente traduzir a página, com a
@@ -56,24 +56,24 @@ export function Home() {
         </p>
         <hr className="border border-maroon-100 w-2/3 mt-10" />
       </div>
-      <div id="section3">
-        <div className="flex flex-row">
-          <div>
-            <div>
-              <h1 className="text-3xl">Jogos Nórdicos</h1>
-              <p>
+      <div id="section3" className="flex items-center justify-center p-5 mx-5">
+        <div className="flex flex-row justify-center items-center gap-52 mbl:flex-col-reverse mbl:gap-20">
+          <div className="flex flex-col gap-10 mbl:items-center mbl:text-center">
+            <div className="flex flex-col gap-7 w-96">
+              <h1 className="text-6xl text-maroon-100 mbl:text-5xl">Jogos Nórdicos</h1>
+              <p className="w-full">
                 Aqui coloquei diversos jogos digitais, que tem como tema
                 principal o mundo viking.
               </p>
             </div>
             <div>
-              <button className="flex items-center justify-center bg-red-400 text-gray-500 rounded p-5">
+              <button className="flex items-center justify-center w-1/2 bg-red-400 text-gray-500 rounded p-3 mbl:w-72 hover:bg-opacity-80">
                 Ver Jogos
               </button>
             </div>
           </div>
           <div>
-            <img src={GamesIllustration} alt="" />
+            <img className="mbl:w-80" src={GamesIllustration} alt="" />
           </div>
         </div>
       </div>
