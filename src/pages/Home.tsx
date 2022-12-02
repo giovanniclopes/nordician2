f50499253e68b44ce48b5c1a0c9ad9336d35d1b8
Giovanni Lopes <giovanniclopes@gmail.com>
2022-12-02 00:37:20 -0300

    'Home' updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ export function Home() {
   return (
     <>
       <Navbar />
-      <div className="h-fit bg-main bg-cover">
+      <div id="section1" className="h-fit bg-main bg-cover">
         <div className="flex items-center justify-center py-40 sm:py-32">
           <div className="flex flex-col items-center justify-center gap-12 font-nordica font-bold">
             <div className="flex flex-col items-center justify-center gap-4">
@@ -18,12 +18,12 @@ export function Home() {
               </p>
               <div className="flex flex-row gap-6 items-center justify-center">
                 <a href="">
-                  <button className="bg-red-500 border-[3px] border-transparent rounded-md text-3xl py-4 px-14 transition-all hover:bg-opacity-80 mbl:text-2xl mbl:px-9 mbl:py-3">
+                  <button className="bg-red-400 text-gray-500 border-[3px] border-transparent rounded-md text-3xl py-4 px-14 transition-all hover:bg-opacity-80 mbl:text-2xl mbl:px-9 mbl:py-3">
                     Mitologia
                   </button>
                 </a>
                 <a href="">
-                  <button className="bg-transparent border-[3px] border-red-500 rounded-md text-red-500 text-3xl py-4 px-14 transition-all hover:border-opacity-80 hover:text-opacity-80 mbl:text-2xl mbl:px-9 mbl:py-3">
+                  <button className="bg-transparent border-[3px] border-red-400 rounded-md text-red-400 text-3xl py-4 px-14 transition-all hover:border-opacity-80 hover:text-opacity-80 mbl:text-2xl mbl:px-9 mbl:py-3">
                     Realidade
                   </button>
                 </a>
@@ -31,6 +31,29 @@ export function Home() {
             </div>
           </div>
         </div>
+      </div>
+      <div
+        id="section2"
+        className="flex flex-col items-center justify-center gap-6 p-12 font-nordica"
+      >
+        <hr className="border border-maroon-100 w-2/3 mb-10" />
+        <p className="text-center w-3/4">
+          Olá viajante, bem vinda(o) ao site Nordician! <br />
+          Aqui forneço diversos artigos para quem busca por informações sobre a
+          cultura nórdica. <br /> Contendo artigos sobre o mundo real, e sobre o
+          mundo místico. <br /> No topo/menu deste site, você encontrará os
+          links:
+          <span className="text-maroon-100"> 'Mitologia'</span> e{" "}
+          <span className="text-maroon-100">'Realidade'</span>, onde estão
+          localizados todo o conteúdo nórdico. Aproveite! <br />
+        </p>
+        <p className="text-center w-3/4 text-maroon-100">
+          E lembrando! os textos de todos os artigos podem estar no idioma
+          Dinamarquês ou Norueguês, para preservar a cultura e dar um aspecto
+          mais nórdico. Porém você pode facilmente traduzir a página, com a
+          solitação que você irá receber do Google Tradutor.
+        </p>
+        <hr className="border border-maroon-100 w-2/3 mt-10" />
       </div>
     </>
   );
