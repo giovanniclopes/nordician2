98008178b3b16d1723de461d7a072c30af99c3e1
Giovanni Lopes <giovanniclopes@gmail.com>
2022-12-10 15:47:02 -0300

    Update Mythology.tsx

--- src/pages/Mythology.tsx
+++ src/pages/Mythology.tsx
@@ -1,6 +1,5 @@
 import { Categories } from "../components/Categories";
 import img1 from "../assets/bg-games.png";
-import img2 from "../assets/gamesIllustration.gif";
 import Navbar from "../components/Navbar";
 import { ScrollToTop } from "../components/ScrollToTop";
 
@@ -13,8 +12,8 @@ export function Mythology() {
         <h1>Mitologia</h1>
         <div className="flex flex-row">
           <Categories title="item1" imageUrl={img1} link="/" />
-          <Categories title="item2" imageUrl={img2} link="/" />
-          <Categories title="item3" imageUrl={img2} link="/" />
+          <Categories title="item2" imageUrl={img1} link="/" />
+          <Categories title="item3" imageUrl={img1} link="/" />
         </div>
       </div>
     </div>
