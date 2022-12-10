58757d17a93e17f8ff0d1104dafed2e6b43c9eb0
Giovanni Lopes <giovanniclopes@gmail.com>
2022-12-10 18:23:32 -0300

    att

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 interface CategorieProps {
   title: string;
@@ -8,20 +8,22 @@ interface CategorieProps {
 
 export function Categories(props: CategorieProps) {
   return (
-    <div className="flex flex-col">
-      <div>
-        <img className="w-32 h-32" src={props.imageUrl} alt="" />
+    <Link to={props.link}>
+      <div className="flex flex-col">
+        <div>
+          <img className="w-32 h-32" src={props.imageUrl} alt="" />
+        </div>
+        <div>
+          <h2 className="text-red-400">{props.title}</h2>
+        </div>
+        <div>
+          <Link to={props.link}>
+            <button>Conhecer</button>
+          </Link>
+        </div>
       </div>
-      <div>
-        <h2 className="text-red-400">{props.title}</h2>
-      </div>
-      <div>
-        <Link to={props.link}>
-          <button>Conhecer</button>
-        </Link>
-      </div>
-    </div>
-  )
+    </Link>
+  );
 }
 
 export default Categories;
