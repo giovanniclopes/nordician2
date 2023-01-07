bbb4864ff259e6e429bcc574fea5d056de12b0f2
Giovanni Lopes <giovanniclopes@gmail.com>
2023-01-07 10:31:32 -0300

    Update Tyr.tsx

--- src/pages/mythology/gods/Tyr.tsx
+++ src/pages/mythology/gods/Tyr.tsx
@@ -8,8 +8,8 @@ export function Tyr() {
       <div>
         <ArticlePage
           imageUrl={TyrImg}
-          title="Tyr"
-          baseText="Týr é um deus da mitologia nórdica conhecido como o deus da guerra e da justiça. Ele é um deus corajoso e valente e é considerado um líder entre os deuses nórdicos. Ele é geralmente retratado como um guerreiro forte e é conhecido por sua determinação e determinação. Na mitologia nórdica, ele é um deus importante e é mencionado em muitas histórias e lendas antigas. Algumas das suas principais habilidades incluem a capacidade de lutar com espadas e a capacidade de fazer justiça. Ele é um deus muito popular e é reverenciado e temido por muitos. Ele é também conhecido por ter um braço artificial feito de prata, que ele perdeu em uma luta contra o lobo Fenrir. Týr é um deus importante na mitologia nórdica e é lembrado como um símbolo de coragem e determinação."
+          title="Týr"
+          baseText="Týr é um deus da mitologia nórdica conhecido como o deus da guerra e da justiça. Ele é um deus corajoso e valente e é considerado um líder entre os deuses nórdicos. Ele é geralmente retratado como um guerreiro forte e é conhecido por sua determinação e determinação. Na mitologia nórdica, ele é um deus importante e é mencionado em muitas histórias e lendas antigas. Algumas das suas principais habilidades incluem a capacidade de lutar com espadas e a capacidade de fazer justiça. Ele é um deus muito popular e é reverenciado e temido por muitos. Ele é também conhecido por ter um braço artificial feito de prata, que ele perdeu em uma luta contra o lobo Fenrir. Týr é um deus importante na mitologia nórdica e é lembrado como um símbolo de coragem e determinação. Além disso, ele é considerado o deus da lealdade e da honra e é venerado por aqueles que lutam por esses valores. Ele é também um deus associado à justiça e é considerado o patrono dos juízes e advogados. Ele é um deus respeitado e admirado por muitos e é lembrado como um símbolo de força e determinação."
         />
       </div>
     </div>
